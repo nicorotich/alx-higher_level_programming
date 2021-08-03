f9eb95ff2567804ab29c292642b30f15c018246e
nicorotich <nicholaskrotich8@gmail.com>
2021-08-03 05:10:52 +0300

    Fix name

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,2 +1,7 @@
 #!/usr/bin/node
-console.log(typeof process.argv[2] === 'undefined' ? 'No argument' : process.argv[2]);
+const firstArg = process.argv[2];
+if (firstArg === undefined) {
+  console.log('No argument');
+} else {
+  console.log(firstArg);
+}
